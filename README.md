0b13000f76d74bdc2e10f254a313bb41f22a4cf6
Feiwenkeke <feiwenkeke@gmail.com>
2017-08-07 13:56:10 +0800

    First
    First

---
+++ README.md
@@ -0,0 +1,3 @@
+# shell
+
+The list of my shell
---
+++ StartMyEnviroment.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
+Info="${Green_font_prefix}[信息]${Font_color_suffix}"
+Error="${Red_font_prefix}[错误]${Font_color_suffix}"
+Tip="${Green_font_prefix}[注意]${Font_color_suffix}"
+
+cd /root/
+Update{
+  echo -e "更新系统${Tip} "
+	echo
+  apt-get update
+  apt-get upgrade
+}
+
+wget -N --no-check-certificate https://softs.fun/Bash/bbr.sh && chmod +x bbr.sh && bash bbr.sh
+curl -sSL http://acs-public-mirror.oss-cn-hangzhou.aliyuncs.com/docker-engine/internet | sh -
